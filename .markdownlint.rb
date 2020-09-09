0ee2e8af767dd08e3a88f1e6300db6fb65990b3a
Squirtle <70493544+squirtleh2o@users.noreply.github.com>
2020-09-09 02:57:38 -0700

    Update .markdownlint.rb

--- .markdownlint.rb
+++ .markdownlint.rb
@@ -3,8 +3,14 @@ all
 # Allow long lines
 exclude_rule 'MD013'
 
+# Allow trailing punctuation in header
+exclude_rule 'MD026'
+
 # Allow inline HTML
 exclude_rule 'MD033'
+
+# Ignore emphasized paragraphs as header
+exclude_rule 'MD036'
 
 # Allow front matter
 exclude_rule 'MD041'
